1edfbe99405da36ceb8b3207e058729458c65e3b
Ally Maumba <amaumba1@gmail.com>
2018-06-04 22:20:18 -0500

    modify profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport'); 
 
+// Load validation 
+const validateProfileInput = require('../../validation/profile');
 
 // Load Profile Model
 const Profile = require('../../models/Profile'); 
@@ -40,6 +42,15 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
 // @access Private
 
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    // Check validation 
+    if(!isValid) {
+        // Return any errors with 400 status
+        return res.status(400).json(errors); 
+    } 
+
     // Get fields 
     const profileFields = {}; 
     profileFields.user = req.user.id; 
