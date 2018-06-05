8673cd86f13b64a769b431b2ede26801f384b7f2
Ally Maumba <amaumba1@gmail.com>
2018-06-04 22:58:50 -0500

    modify models schema

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -26,6 +26,7 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
     const errors = {}; 
 
     Profile.findOne({ user: req.user.id })
+        .populate('user', ['name','avatar'])
         .then(profile => {
             if(!profile) {
 
