d0ea360c00a54d6c7d7940be9e7ace687d18e8ca
Ally Maumba <amaumba1@gmail.com>
2018-06-07 20:42:06 -0500

    added experience validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 
 // Load validation 
 const validateProfileInput = require('../../validation/profile');
+const validateExperienceInput = require('../../validation/experience');
 
 // Load Profile Model
 const Profile = require('../../models/Profile'); 
@@ -167,6 +168,14 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
 // @access Private 
 
 router.post('/experience', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const { errors, isValid } = validateExperienceInput(req.body);
+
+    // Check Validation
+    if(!isValid) {
+        //Return any errors with 400 status
+        return res.status(400).json(errors); 
+    }
+
     Profile.findOne({ user: req.user.id })
         .then(profile => {
             const newExp = {
