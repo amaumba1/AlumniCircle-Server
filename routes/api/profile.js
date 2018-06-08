0a93ae60780c800561df6b6f4eacb4f39043a964
Ally Maumba <amaumba1@gmail.com>
2018-06-08 07:08:32 -0500

    add education router and education  validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,7 @@ const passport = require('passport');
 // Load validation 
 const validateProfileInput = require('../../validation/profile');
 const validateExperienceInput = require('../../validation/experience');
+const validateEducationInput = require('../../validation/education');
 
 // Load Profile Model
 const Profile = require('../../models/Profile'); 
@@ -198,5 +199,40 @@ router.post('/experience', passport.authenticate('jwt', { session: false }), (re
        
 })
 
+
+// @route  POST api/profile/education  
+// @desc   Add education to profile  
+// @access Private 
+
+router.post('/education', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const { errors, isValid } = validateEducationInput(req.body);
+
+    //Check validation 
+    if(!isValid) {
+        // Return any errors with 400 status
+        return res.status(400).json(errors);
+    }
+
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            const newEdu = {
+                school: req.body.school,
+                degree: req.body.degree,
+                fieldofstudy: req.body.fieldofstudy,
+                from: req.body.from,
+                to: req.body.to,
+                current: req.body.current,
+                description: req.body.description
+            }
+
+            // Add to exp array 
+            // use ushift to add education at the beginning 
+            // when use push will put it at the end
+            profile.education.unshift(newEdu);
+
+            profile.save().then(profile => res.json(profile));
+        })
+
+})
 
 module.exports = router;
